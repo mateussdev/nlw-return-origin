99a16e4f011d0f014c9ffb6ba0a7a8e6263425ea
Mateus Silva <mateusbs427@gmail.com>
2022-05-21 17:07:23 -0300

    :pencil: Class 2
    Navigation and menu

---
+++ main.js
@@ -0,0 +1,17 @@
+function onScroll() {
+  const navigation = document.querySelector('#navigation');
+
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  }else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}

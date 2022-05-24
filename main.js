4d4a6fca5516223ecd7bcad9c7d9b81d5a13d7a6
Mateus Silva <mateusbs427@gmail.com>
2022-05-24 20:15:34 -0300

    :pencil: Class 4
    Contact section;
    Standardize buttons;
    Footer;
    Back to top button;
    Page color changes(svg: fill, stroke);
    Whatsapp contact link

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-function onScroll() {
-  const navigation = document.querySelector('#navigation');
+window.addEventListener('scroll', onScroll)
 
+onScroll()
+
+function onScroll() {
+  showNavOnScroll()
+  showBakcToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
-  }else {
+  } else {
     navigation.classList.remove('scroll')
   }
 }
 
+function showBakcToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
 function openMenu() {
-  document.body.classList.add('menu-expanded');
+  document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded');
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
+  duration: 700
 }).reveal(`
   #home, 
   #home img,
@@ -29,4 +44,4 @@ ScrollReveal({
   #about,
   #about header,
   #about .content
-`);
+`)
